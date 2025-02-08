979d9cedda37494bba46d3ac935d68163444b914
alastafew <alexastafew@gmail.com>
2025-02-08 20:54:10 +0700

    Initial Commit

---
+++ 46-2-4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  46-2-4
+//
+//  Created by Ast on 08.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
